a83c3749134af8f97be0dd9172c2c801dcc137bb
pnadiadhara <priteshrna@gmail.com>
2018-11-14 14:23:44 -0500

    Initial Push

---
+++ AutoLayoutLab/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AutoLayoutLab
+//
+//  Created by Pritesh Nadiadhara on 11/14/18.
+//  Copyright © 2018 Pritesh Nadiadhara. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
